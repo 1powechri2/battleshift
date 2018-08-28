b3a94d47782f5e50afc3d0969c9fe22df04335f7
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-08-28 08:44:45 -0600

    adds user migration

--- db/migrate/20180828032240_create_users.rb
+++ db/migrate/20180828032240_create_users.rb
@@ -2,8 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :email
       t.string :password_digest
-      t.boolean :email_confirmed
+      t.boolean :email_confirmed, default: false
       t.string :confirm_token
 
       t.timestamps
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
     name "MyString"
+    email "MyString"
     password_digest "MyString"
     email_confirmed false
     confirm_token "MyString"
